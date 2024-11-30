3e0b311655ad9466d5b07c01bd3e74d62bbbd702
Suraj Iyer <iyersuraj7@gmail.com>
2024-12-01 00:49:19 +0530

    added database

--- main.py
+++ main.py
@@ -2,11 +2,35 @@ from flask import Flask, render_template, url_for, flash, redirect, request, ses
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+from flask_sqlalchemy import SQLAlchemy
 from forms import CreateAgentForm, CreateRoomForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1f5d157db8961410fa567f402f995634'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
+
+class Agent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    sum_of_ratings = db.Column(db.Integer, default=0)
+    no_of_ratings = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f'Agent with id {self.id} and name {self.name}'
+    
+class Query(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    query = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    level = db.Column(db.Integer, nullable=False)
+    agent_assigned = db.Column(db.String(20), nullable=False)
+    code = db.Column(db.String(4), nullable=False)
+    
+    def __repr__(self):
+        return f'Query with id {self.id}, level {self.level}, assigned to {self.agent_assigned}, code {self.code}, asked at {self.query}'
 
 agentdb = [
     {
