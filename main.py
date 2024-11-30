67e9eed6de5e1ac778fe7458001573b591eb8f46
Suraj Iyer <iyersuraj7@gmail.com>
2024-11-30 17:32:22 +0530

    home page done

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+agents = [
+    {
+        'name': 'General'
+    },
+    {
+        'name': 'Suraj'
+    },
+    {
+        'name': 'Pooraj'
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h2>Hello world!</h2>'
+    return render_template('home.html', agents=agents)
 
 if __name__ == '__main__':
     app.run(debug=True)
