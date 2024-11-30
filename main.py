43e2aeef3a74f55f8650a32c5d995d5c9b4d79c8
Suraj Iyer <iyersuraj7@gmail.com>
2024-11-30 20:52:57 +0530

    before socketio

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import CreateAgentForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1f5d157db8961410fa567f402f995634'
 
-agents = [
+agentdb = [
     {
-        'name': 'General'
+        'name': 'General',
     },
     {
-        'name': 'Suraj'
+        'name': 'Suraj',
     },
     {
-        'name': 'Pooraj'
+        'name': 'Pooraj',
     },
 ]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', agents=agents)
+    return render_template('home.html')
+
+@app.route('/agents')
+def agents():
+    return render_template('agents.html', agentdb=agentdb)
+
+@app.route('/create-agent')
+def create_agent():
+    form = CreateAgentForm()
+    return render_template('create_agent.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
