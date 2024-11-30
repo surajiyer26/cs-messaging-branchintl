64e7e165c95d710c8047a79c57005f209a266967
Suraj Iyer <iyersuraj7@gmail.com>
2024-11-30 23:06:26 +0530

    got the forms working (socketio work not really started yet)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect, request, session
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
 from forms import CreateAgentForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1f5d157db8961410fa567f402f995634'
+socketio = SocketIO(app)
 
 agentdb = [
     {
@@ -25,10 +29,20 @@ def home():
 def agents():
     return render_template('agents.html', agentdb=agentdb)
 
-@app.route('/create-agent')
+@app.route('/create-agent', methods=['GET', 'POST'])
 def create_agent():
     form = CreateAgentForm()
+    if form.validate_on_submit():
+        if {'name': form.name.data} in agentdb:
+            flash(f'Agent with name {form.name.data} already exists.', 'warning')
+        else:
+            agentdb.append({'name': form.name.data})
+            flash(f'Agent with name {form.name.data} created.', 'success')
+            return redirect(url_for('agents'))
+    else:
+        if request.method == 'POST':
+            flash('Form validation failed. Please check your input.', 'danger')
     return render_template('create_agent.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
